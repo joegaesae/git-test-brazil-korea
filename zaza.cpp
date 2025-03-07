eb24251be772d87a317bad0a315fa6106624dc83
Joao Chede <joao.lealchede@epfl.ch>
2025-03-07 13:39:56 +0100

    sss

--- zaza.cpp
+++ zaza.cpp
@@ -11,3 +11,5 @@ this is the third change: caca
 Korea <<  Brazil
 
 all the countries are equal(but not Brazil: Brazil is simply better than the rest, specially better than South Korea)
+
+static_assert
